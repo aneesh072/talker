5dc2fa354038e9bc3c9c4fd5db4cfd69d86084d8
Enis Karki <aneeshkarki292@gmail.com>
2023-02-05 19:42:29 +1100

    added chat between two users

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,16 @@
 import React, { useContext, useState } from 'react';
-import {AuthContext} from '../context/AuthContext'
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { AuthContext } from '../context/AuthContext';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 import { db } from '../firebase';
 
 const Search = () => {
@@ -30,6 +40,43 @@ const Search = () => {
     e.code === 'Enter' && handleSearch();
   };
 
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    try {
+      const res = await getDoc(doc(db, 'chats', combinedId));
+
+      if (!res.exists()) {
+        await setDoc(doc(db, 'chats', combinedId), { messages: [] });
+
+        //create use r chats
+
+        await updateDoc(doc(db, 'userChats', currentUser.uid), {
+          [combinedId + '.userInfo']: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + '.date']: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, 'userChats', user.uid), {
+          [combinedId + '.userInfo']: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + '.date']: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+    setUser(null);
+    setUsername('');
+  };
+
   return (
     <div className="search">
       <div className="search-form">
@@ -38,11 +85,12 @@ const Search = () => {
           placeholder="Search user.."
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
       </div>
       {err && <span>Something went wrong</span>}
       {user && (
-        <div className="user-chat">
+        <div className="user-chat" onClick={handleSelect}>
           <img src={user.photoURL} alt="" />
           <div className="user-chat-info">
             <span>{user.displayName}</span>
