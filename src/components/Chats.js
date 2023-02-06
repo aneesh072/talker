6ef9ff616a6e1b268bc678b13082b525173387c9
Enis Karki <aneeshkarki292@gmail.com>
2023-02-06 12:24:37 +1100

    saved user chat user to sidebar

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase';
 import Pug from '../assets/pug.jpg';
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  console.log(chats);
   return (
-    <div>
-      <div className="user-chat">
-        <img src={Pug} alt="" />
-        <div className="user-chat-info">
-          <span>Ron</span>
-          <span className="p">Last meassgae</span>
+    <div className="chats">
+      {Object.entries(chats)?.map((chat) => (
+        <div className="user-chat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="user-chat-info">
+            <span>{chat[1].userInfo.displayName}</span>
+            <span className="p">{chat[1].userInfo.lastMessage?.text}</span>
+          </div>
         </div>
-      </div>
-
-      <div className="user-chat">
-        <img src={Pug} alt="" />
-        <div className="user-chat-info">
-          <span>Ron</span>
-          <span className="p">Last meassgae</span>
-        </div>
-      </div>
-
-      <div className="user-chat">
-        <img src={Pug} alt="" />
-        <div className="user-chat-info">
-          <span>Ron</span>
-          <span className="p">Last meassasdasda asd asd asdgae</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
