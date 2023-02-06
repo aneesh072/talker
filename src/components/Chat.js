8b4ff8078962678c0a8c7d9663b0f8cb1b82f1bd
Enis Karki <aneeshkarki292@gmail.com>
2023-02-06 12:38:57 +1100

    set user name on chat header

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Input from './Input';
 import Messages from './Messages';
+import { ChatContext } from '../context/ChatContext';
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className="chat">
       <div className="chat-info">
-        <span>Tyler</span>
+        <span>{data.user.displayName}</span>
       </div>
       <Messages />
       <Input />
--- src/components/Chats.js
+++ src/components/Chats.js
@@ -3,11 +3,13 @@ import { AuthContext } from '../context/AuthContext';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase';
 import Pug from '../assets/pug.jpg';
+import { ChatContext } from '../context/ChatContext';
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
 
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -23,11 +25,19 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
+  const handleSelect = (u) => {
+    dispatch({ type: 'CHANGE_USER', payload: u });
+  };
+
   console.log(chats);
   return (
     <div className="chats">
       {Object.entries(chats)?.map((chat) => (
-        <div className="user-chat" key={chat[0]}>
+        <div
+          className="user-chat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="user-chat-info">
             <span>{chat[1].userInfo.displayName}</span>
